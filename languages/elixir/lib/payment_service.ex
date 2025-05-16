86214411541b76bea8f6a477cf45fc05b5f6363b
muneeb-programmer <muneeb@expedey.com>
2025-05-16 16:50:35 +0500

    Update payment_service.ex

--- languages/elixir/lib/payment_service.ex
+++ languages/elixir/lib/payment_service.ex
@@ -1,7 +1,7 @@
 defmodule PaymentService do
   # AWS credentials in module attribute
   @aws_access_key "AKIAIOSFODNN7EXAMPLE"
-  @aws_secret_key "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
   
   # Stripe API key in module attribute
   @stripe_key "sk_live_51HqX9K2J3k4L5m6n7o8p9q0r1s2t3u4v5w6x7y8z9"

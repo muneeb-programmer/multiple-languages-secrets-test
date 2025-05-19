d1611fe79a8925c53af692f470eae1cf3dcd9cf2
muneeb-programmer <muneeb@expedey.com>
2025-05-19 15:53:58 +0500

    Update PaymentService.cs

--- languages/dotnet/Services/PaymentService.cs
+++ languages/dotnet/Services/PaymentService.cs
@@ -2,7 +2,7 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
-namespace PaymentProcessor.Se2
+namespace PaymentProcessor.test
 {
     public class PaymentService
     {
